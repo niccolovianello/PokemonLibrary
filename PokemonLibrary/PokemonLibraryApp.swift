0bf6a8ab37169046da693607d7926249c32b1da0
Niccolò Vianello <n.vianello@reply.it>
2025-06-06 19:40:57 +0200

    Initial Commit

---
+++ PokemonLibrary/PokemonLibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonLibraryApp.swift
+//  PokemonLibrary
+//
+//  Created by Niccolò Vianello on 06/06/25.
+//
+
+import SwiftUI
+
+@main
+struct PokemonLibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
